d00a2f890fef974226ec3eb33d2190f7c7b3c422
chlsgong <chlsgong@gmail.com>
2020-05-22 01:49:37 -0500

    add socket handler

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-const app = require('express')()
-const server = require('http').createServer(app)
+const app = require('express')();
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 // Load configurations
-const config = require('./config.json')
-
-// Start server
-const { port } = config
-server.listen(port, () => {
-  console.log(`App listening on port ${port}!`)
-})
+const config = require('./config.json');
 
 // Socket
 
-io.on('connection', (socket) => {
+io.on('connect', socket => {
   // TODO: do something with socket
   console.log('A user connected');
+
+  socket.on('add-to-queue', data => {
+    console.log(data);
+  })
 });
 
 // Endpoints
 
 app.get('/', (_, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 // TODO: create room
 // TOOD: join room
+
+// Start server
+const { port } = config;
+server.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
